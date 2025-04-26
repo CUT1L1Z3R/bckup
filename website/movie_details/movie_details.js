41ef1ce85da0e8440e7eb257a98ff364fef3c173
CUT1L1Z3R <151803749+CUT1L1Z3R@users.noreply.github.com>
2025-04-26 09:33:22 +0800

    Update movie_details.js

--- website/movie_details/movie_details.js
+++ website/movie_details/movie_details.js
@@ -38,14 +38,27 @@ async function fetchVideoDetails(id) {
     return data.results;
 }
 
-document.getElementById('change-server-btn').addEventListener('click', () => {
+let isButtonHeldDown = false;
+
+document.getElementById('change-server-btn').addEventListener('mousedown', () => {
+  isButtonHeldDown = true;
   document.getElementById('server-selector').style.display = 'block';
 });
 
 document.getElementById('server-selector').addEventListener('click', () => {
   document.getElementById('server-selector').style.display = 'none';
+  isButtonHeldDown = false;
 });
 
+document.getElementById('server-selector').addEventListener('mouseup', () => {
+  document.getElementById('server-selector').style.display = 'none';
+  isButtonHeldDown = false;
+});
+
+document.getElementById('server-selector').addEventListener('mouseleave', () => {
+  document.getElementById('server-selector').style.display = 'none';
+  isButtonHeldDown = false;
+});
 // Function to handle video source change based on selected server
 async function changeServer() {
     const server = document.getElementById('server').value; // Get the selected server
